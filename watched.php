562eeeca6e31b8291a858c88a5521410e53346e0
rohitsingh <rohitsingh123454545@gmail.com>
2021-01-26 10:04:38 +0530

    Update watched.php

--- watched.php
+++ watched.php
@@ -9,7 +9,7 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>YaarMe</title>
+    <title>Views</title>
     <link rel="stylesheet" href="./CSS/style.css">
     <link rel="stylesheet" href="page/css/like.css">
     <!--icons-->
@@ -331,7 +331,7 @@ session_start();
            <svg enable-background="new 0 0 64 64" viewBox="0 0 64 64" data-supported-dps="24x24" fill="black" width="24" height="24" xmlns="http://www.w3.org/2000/svg"><path d="m54 30h-39.899l15.278-14.552c.8-.762.831-2.028.069-2.828-.761-.799-2.027-.831-2.828-.069l-17.448 16.62c-.755.756-1.172 1.76-1.172 2.829 0 1.068.417 2.073 1.207 2.862l17.414 16.586c.387.369.883.552 1.379.552.528 0 1.056-.208 1.449-.621.762-.8.731-2.065-.069-2.827l-15.342-14.552h39.962c1.104 0 2-.896 2-2s-.896-2-2-2z"></path></svg>
         </a>
         <span class="">
-            <label class="white" for="button_post_desk">Likes</label>
+            <label class="white" for="button_post_desk">Views</label>
         </span>
         <form class="input-wrap" autocomplete="off">
         </form>
