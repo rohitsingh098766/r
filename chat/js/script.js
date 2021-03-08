b13c924847216e72d2c02d1e8cb62781dd837fae
rohitsingh <rohitsingh123454545@gmail.com>
2021-03-08 21:40:02 +0530

    ee

--- chat/js/script.js
+++ chat/js/script.js
@@ -26,6 +26,8 @@
    var inputBox = document.getElementById("input_text");
    var inputForm = document.getElementById('send_message_input');
    var max_id = 1;
+   var last_seen = 0;
+ var get_message_id = 0;
 
    //        send postMessage
    inputForm.addEventListener("submit", function (e) {
@@ -62,32 +64,50 @@
        xhttp.onreadystatechange = function () {
            if (this.readyState == 4 && this.status == 200) {
                if (container) {
+//                   console.log(this.responseText);
                    document.getElementById('conversation_container').insertAdjacentHTML("beforeend", this.responseText);
 
-                                       var incorrect_time = document.querySelectorAll('.time_incorrect');
-                                      for(var i = 0; i < incorrect_time.length; i++){
-                                          
-                                          var currentdate_script = new Date();
-                                          var hours = 0;
-                                          var an_or_pm = 'AM';
-                                          if (currentdate_script.getHours() == 12) {
-                                              hours_script = 12;
-                                              an_or_pm = 'PM';
-                                          } else if (currentdate_script.getHours() > 12) {
-                                              hours_script = currentdate_script.getHours() % 12;
-                                              an_or_pm = 'PM';
-                                          } else {
-                                              hours_script = currentdate_script.getHours();
-                                              an_or_pm = 'AM';
-                                          }
+                   var incorrect_time = document.querySelectorAll('.time_incorrect');
+                   for (var i = 0; i < incorrect_time.length; i++) {
 
-                                          incorrect_time[i].innerHTML = hours_script + ':' + currentdate_script.getMinutes() + ' ' + an_or_pm;
-                                          incorrect_time[i].classList.remove('.time_incorrect');
-                                      }
+                       var currentdate_script = new Date();
+                       var hours = 0;
+                       var an_or_pm = 'AM';
+                       if (currentdate_script.getHours() == 12) {
+                           hours_script = 12;
+                           an_or_pm = 'PM';
+                       } else if (currentdate_script.getHours() > 12) {
+                           hours_script = currentdate_script.getHours() % 12;
+                           an_or_pm = 'PM';
+                       } else {
+                           hours_script = currentdate_script.getHours();
+                           an_or_pm = 'AM';
+                       }
 
+                       incorrect_time[i].innerHTML = hours_script + ':' + currentdate_script.getMinutes() + ' ' + an_or_pm;
+                       incorrect_time[i].classList.remove('.time_incorrect');
+                   }
 
+                   var update_tick = document.querySelectorAll('.data');
+                   for (var i = 0; i < update_tick.length; i++) {
+                       var max_last_seen = update_tick[i].getAttribute('last_seen');
+                       if (max_last_seen > last_seen) {
+                           last_seen = max_last_seen;
+                           update_tick[i].classList.remove('data');
+                           console.log(last_seen);
+                       }
+                   }
+                   var correct_mark = document.querySelectorAll('.unread_messages')
+                   for (var i = 0; i < correct_mark.length; i++) {
+                       get_message_id = correct_mark[i].getAttribute('message');
+                       if (get_message_id <= last_seen) {
+                           correct_mark[i].innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="15" id="msg-dblcheck-ack" x="2063" y="2076"><path d="M15.01 3.316l-.478-.372a.365.365 0 0 0-.51.063L8.666 9.88a.32.32 0 0 1-.484.032l-.358-.325a.32.32 0 0 0-.484.032l-.378.48a.418.418 0 0 0 .036.54l1.32 1.267a.32.32 0 0 0 .484-.034l6.272-8.048a.366.366 0 0 0-.064-.512zm-4.1 0l-.478-.372a.365.365 0 0 0-.51.063L4.566 9.88a.32.32 0 0 1-.484.032L1.892 7.77a.366.366 0 0 0-.516.005l-.423.433a.364.364 0 0 0 .006.514l3.255 3.185a.32.32 0 0 0 .484-.033l6.272-8.048a.365.365 0 0 0-.063-.51z" fill="#009be0" /></svg>';
+                       }
+                      
+                   }
+console.log('get_message_id:'+get_message_id+'  and last_seen: '+last_seen)
 
-                   if (this.responseText.length > 10) {
+                   if (this.responseText.length > 100) {
                        document.getElementById('insert_here').innerHTML = "";
                        scrollToBottom();
                    }
@@ -98,7 +118,7 @@
                    my_ajax("more_message.php", 'room=' + room + '&max_id=' + last_message_id(), 'insert_here');
                }
            }
-           scrollToBottom();
+           //           scrollToBottom();
        };
        xhttp.open("POST", url, true);
        xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
--- chat/more_message.php
+++ chat/more_message.php
@@ -42,12 +42,12 @@ $opponent_last_seen_time =  $row_seen['last_login'];
     $side = 'sent';
    if($opponent_last_seen_message >= $row_room['id']){
         
-   $revienced_icon =  '<span class="tick"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="15" id="msg-dblcheck-ack" x="2063" y="2076"><path d="M15.01 3.316l-.478-.372a.365.365 0 0 0-.51.063L8.666 9.88a.32.32 0 0 1-.484.032l-.358-.325a.32.32 0 0 0-.484.032l-.378.48a.418.418 0 0 0 .036.54l1.32 1.267a.32.32 0 0 0 .484-.034l6.272-8.048a.366.366 0 0 0-.064-.512zm-4.1 0l-.478-.372a.365.365 0 0 0-.51.063L4.566 9.88a.32.32 0 0 1-.484.032L1.892 7.77a.366.366 0 0 0-.516.005l-.423.433a.364.364 0 0 0 .006.514l3.255 3.185a.32.32 0 0 0 .484-.033l6.272-8.048a.365.365 0 0 0-.063-.51z" fill="#009be0" /></svg></span>';
+   $revienced_icon =  '<span class="tick" message="'.$row_room['id'].'"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="15" id="msg-dblcheck-ack" x="2063" y="2076"><path d="M15.01 3.316l-.478-.372a.365.365 0 0 0-.51.063L8.666 9.88a.32.32 0 0 1-.484.032l-.358-.325a.32.32 0 0 0-.484.032l-.378.48a.418.418 0 0 0 .036.54l1.32 1.267a.32.32 0 0 0 .484-.034l6.272-8.048a.366.366 0 0 0-.064-.512zm-4.1 0l-.478-.372a.365.365 0 0 0-.51.063L4.566 9.88a.32.32 0 0 1-.484.032L1.892 7.77a.366.366 0 0 0-.516.005l-.423.433a.364.364 0 0 0 .006.514l3.255 3.185a.32.32 0 0 0 .484-.033l6.272-8.048a.365.365 0 0 0-.063-.51z" fill="#009be0" /></svg></span>';
     }else if($opponent_last_seen_message >= $row_room['id']){
         
-   $revienced_icon =  '<span class="tick"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="15" id="msg-dblcheck-ack" x="2063" y="2076"><path d="M15.01 3.316l-.478-.372a.365.365 0 0 0-.51.063L8.666 9.88a.32.32 0 0 1-.484.032l-.358-.325a.32.32 0 0 0-.484.032l-.378.48a.418.418 0 0 0 .036.54l1.32 1.267a.32.32 0 0 0 .484-.034l6.272-8.048a.366.366 0 0 0-.064-.512zm-4.1 0l-.478-.372a.365.365 0 0 0-.51.063L4.566 9.88a.32.32 0 0 1-.484.032L1.892 7.77a.366.366 0 0 0-.516.005l-.423.433a.364.364 0 0 0 .006.514l3.255 3.185a.32.32 0 0 0 .484-.033l6.272-8.048a.365.365 0 0 0-.063-.51z" fill="#9b9b9b" /></svg></span>';
+   $revienced_icon =  '<span class="tick" message="'.$row_room['id'].'"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="15" id="msg-dblcheck-ack" x="2063" y="2076"><path d="M15.01 3.316l-.478-.372a.365.365 0 0 0-.51.063L8.666 9.88a.32.32 0 0 1-.484.032l-.358-.325a.32.32 0 0 0-.484.032l-.378.48a.418.418 0 0 0 .036.54l1.32 1.267a.32.32 0 0 0 .484-.034l6.272-8.048a.366.366 0 0 0-.064-.512zm-4.1 0l-.478-.372a.365.365 0 0 0-.51.063L4.566 9.88a.32.32 0 0 1-.484.032L1.892 7.77a.366.366 0 0 0-.516.005l-.423.433a.364.364 0 0 0 .006.514l3.255 3.185a.32.32 0 0 0 .484-.033l6.272-8.048a.365.365 0 0 0-.063-.51z" fill="#9b9b9b" /></svg></span>';
     }else{
-   $revienced_icon =  '<span class="tick"><svg aria-hidden="true" data-prefix="fal" data-icon="check" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="svg-inline--fa fa-check fa-w-14 fa-7x" width="12" height="12"><path fill="#9b9b9b" d="M413.505 91.951L133.49 371.966l-98.995-98.995c-4.686-4.686-12.284-4.686-16.971 0L6.211 284.284c-4.686 4.686-4.686 12.284 0 16.971l118.794 118.794c4.686 4.686 12.284 4.686 16.971 0l299.813-299.813c4.686-4.686 4.686-12.284 0-16.971l-11.314-11.314c-4.686-4.686-12.284-4.686-16.97 0z" class=""></path></svg></span>';
+   $revienced_icon =  '<span class="tick unread_messages" message="'.$row_room['id'].'"><svg aria-hidden="true" data-prefix="fal" data-icon="check" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="svg-inline--fa fa-check fa-w-14 fa-7x" width="12" height="12"><path fill="#9b9b9b" d="M413.505 91.951L133.49 371.966l-98.995-98.995c-4.686-4.686-12.284-4.686-16.971 0L6.211 284.284c-4.686 4.686-4.686 12.284 0 16.971l118.794 118.794c4.686 4.686 12.284 4.686 16.971 0l299.813-299.813c4.686-4.686 4.686-12.284 0-16.971l-11.314-11.314c-4.686-4.686-12.284-4.686-16.97 0z" class=""></path></svg></span>';
         
     }
 }else{
@@ -66,7 +66,7 @@ $opponent_last_seen_time =  $row_seen['last_login'];
          
          
      }
-     echo $final_result;
+     echo $final_result.'<div class="data" last_seen="'.$opponent_last_seen_message.'"></div>';
 if($update_last_seen_message===true){
     
           $query_insert = "UPDATE `yaarme_message`.`my_room` SET `last_seen_msg_id` = '{$max_message_id}' WHERE (room_id = {$room} and room_member = {$_SESSION['id']});";
