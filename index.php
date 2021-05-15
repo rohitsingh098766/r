dab6fb3968b3bea10b55d62da09d91e5da9ebd2f
rohitsingh <rohitsingh123454545@gmail.com>
2021-05-15 14:05:22 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1274,7 +1274,7 @@ for(var i = 0; i < images.length; i++){
 
     <div class="mobile-nav-bar">
         <ul class="nav-icons">
-            <a href="#" class="icon home-icon home-icon-active" title="Home">
+            <a href="./" class="icon home-icon home-icon-active" title="Home">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="30px" height="30px">
                     <path d="M 12 2.0996094 L 1 12 L 4 12 L 4 21 L 10 21 L 10 15 L 14 15 L 14 21 L 20 21 L 20 12 L 23 12 L 12 2.0996094 z"></path>
                 </svg>
